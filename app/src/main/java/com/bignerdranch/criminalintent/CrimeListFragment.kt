905b19461a171936438a05db77fd2691405bd9ae
Oleg S. Plesser <oleg@plesser.ru>
2022-05-09 16:50:47 +0300

    add imageView

--- app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -77,11 +78,17 @@ class CrimeListFragment : Fragment() {
 
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
 
         fun bind(crime: Crime){
             this.crime = crime
             titleTextView.text = this.crime.title
             dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if (crime.isSolved){
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
